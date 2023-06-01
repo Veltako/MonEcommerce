651495f9106fc43099f97d5ab351b769ece94c55
Veltako <116906054+Veltako@users.noreply.github.com>
2023-06-01 12:18:51 +0200

    ttt

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -7,20 +7,21 @@ Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../')->load();
 //--------- BDD
 $mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 if ($mysqli->connect_error) die('Un problème est survenu lors de la tentative de connexion à la BDD : ' . $mysqli->connect_error);
-// $mysqli->set_charset("utf8");
+$mysqli->set_charset("utf8");
+
 //--------- SESSION
 session_name("monecommerceID");
 session_start();
 //--------- CHEMIN
-// define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");
-define("RACINE_SITE", "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . "/");
+ define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");
+//define("RACINE_SITE", "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . "/");
 
 //--------- VARIABLES
 // $contenu = '';
 
 //--------- AUTRES INCLUSIONS
 require_once("functions.inc.php");
-// debug($mysqli, 0);
+//debug($mysqli, 0);
 // Déclarer une variable d’environnement
 // putenv('NOM_VARIABLE=Le nom de ma variable d\'environnement avec <span style="font-weight:bold;">putenv(NOM_VARIABLE)</span>');
 // // ou
